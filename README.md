3a5e60b5c218884432dfea97517658d35398b832
Katherine Hackett <kathhack@gmail.com>
2024-08-16 20:22:29 +0000

    add intro_practice.py and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 506-practice-workflow
+## Summary
+Number Variable: a variable with a number (44)
+String Variable: a variable with a string (Feed me ice cream)
+List Variable: a variable with a list of snacks
+Dictionary Variable: a dictionary with key value pairs, a list, a nested dictionary
+
+###Function: Analyze Data
+Description: compare two numbers
+Expected output: if 'input1' is 12.32 and 'input 2' is 7.11, the output will be '12.32 is greater than 7.11'
--- intro_practice.py
+++ intro_practice.py
@@ -4,3 +4,41 @@ import numpy as np
 string_var = "Feed me ice cream"
 number_var = '44' 
 Snack_name_list = ['cookies', 'pretzels', 'chocolate']
+#Dictionary
+Practice_dictionary : [{
+    'first_name' : 'Katherine',
+    'last_name' : 'Hackett',
+    'age' : '36',
+    'colors' : ['blue', 'yellow', 'green'],
+    'demographics' : {
+        'sex' : 'female',
+        'race' : 'white'},
+    'address' : {
+        'address' : '92 Leeward Lane',
+        'city' : 'Port Jefferson',
+        'zip' : '11777',}}]
+#Snack prices
+#If then statement
+snack_prices = [12.32, 7.11, 5.27, 10.99]
+print(Practice_dictionary)
+def analyze_data(12.32, 7.11, 5.27, 10.99)
+if 12.32 is greater than 7.11
+result = buy second option
+result
+input1 = 12.32
+input2 = 7.11    
+def analyze_data(input1, input2)
+    if input1 > input2
+        result = f"{input1} is greater than {input2}"
+    else:
+        result = f"{input1} is less than or equal to {input2}"
+return result
+
+print("number variable:", number_var)
+print("String variable:", string_var)
+print("list variable:", list_var)
+print("Dictionary variable:", dict_var)
+
+result = analyze_data (12.32, 7.11)
+print("Function output:", result)   
+
